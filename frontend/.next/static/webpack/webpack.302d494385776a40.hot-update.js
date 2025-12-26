e78459a30f02d490bd9cbd0600b8b60b1f6ac0ab
Sneha Bejugam <snehabejugam08@gmail.com>
2025-12-26 16:44:35 -0500

    Added env to gitignore

--- frontend/.next/static/webpack/webpack.302d494385776a40.hot-update.js
+++ frontend/.next/static/webpack/webpack.302d494385776a40.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "b24563ada8fb5ebc"; }
+/******/ 	__webpack_require__.h = function() { return "f9ac8fb42c827bed"; }
 /******/ }();
 /******/ 
 /******/ }
